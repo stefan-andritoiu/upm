e2169f2895158960c26bcc80168b3710face3ec3
UPSquared <mihai.t.gh.stefanescu@gmail.com>
2018-04-25 19:44:16 +0300

    UpmObject:string initialization initial commit

--- examples/c++/initio.cxx
+++ examples/c++/initio.cxx
@@ -1,8 +1,21 @@
-#include "upm/initioSampleModule.hpp"
+#include "upm/interfaces/UpmObject.hpp"
+#include <string>
+#include <iostream>
 
 using namespace upm;
 
+class MockSensor : public UpmObject
+{
+public:
+  MockSensor(std::string initStr) : UpmObject(initStr) {}
+  MockSensor() : UpmObject() {}
+  ~MockSensor() {}
+};
+
 int main()
 {
-    initioSampleModule gpioModule("g:102:1-upm");
+  /* Example using gpio pin 463 on UP2 Board*/
+  MockSensor mockSensor("g:5:1-upm_attr1:upm_attr2:upm_attr3");
+
+  return 0;
 }
