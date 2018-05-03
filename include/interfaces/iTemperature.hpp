4eada950a501b01873061e7c3c2eaf7dcf99c3f1
UPSquared <mihai.t.gh.stefanescu@gmail.com>
2018-05-03 17:45:30 +0300

    intial commit for iTemperature interface

---
+++ include/interfaces/iTemperature.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+
+namespace upm
+{
+  enum TemperatureType
+  {
+    Celsius = 0,
+    Fahrenheit = 1,
+    Kelvin = 2,
+    custom
+  };
+
+  class iTemperature
+  {
+  public:
+    virtual ~iTemperature() {}
+    virtual float getTemperature() = 0;
+    static float convertTo(TemperatureType type) { return 0.0f; }
+  };
+}
